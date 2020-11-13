686d0ca74f5d41279a9e38dd62b29e1f8fb9837c
crisostomon <72739646+crisostomon@users.noreply.github.com>
2020-11-13 16:51:02 +0000

    fix: Adds slack message for release

--- release.config.js
+++ release.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  branches: "master",
-  repositoryUrl: "https://github.com/crisostomon/react-app",
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/github",
-      {
-        assets: [
-          { path: "build.zip", label: "Build" },
-          { path: "coverage.zip", label: "Coverage Report" },
+    branches: "master",
+    repositoryUrl: "https://github.com/crisostomon/react-app",
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        [
+            "@semantic-release/github",
+            {
+                assets: [
+                    { path: "build.zip", label: "Build" },
+                    { path: "coverage.zip", label: "Coverage Report" },
+                ],
+            },
         ],
-      },
     ],
-  ],
 };
